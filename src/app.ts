8b99b914d70ffdb0948bc5c69692bfc37123cfdf
Nick Derevjanik <nick@networkbasedsolutions.com>
2024-06-02 16:36:55 -0700

    chore: initial express configuration

--- src/app.ts
+++ src/app.ts
@@ -8,5 +8,5 @@ app.get("/", (_req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`App is available at: http://localhost:${port}`);
 });
