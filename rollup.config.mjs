28b6a6c3f2c18804b82e45952f2756a1ca937a07
Nick Derevjanik <nick@networkbasedsolutions.com>
2024-06-23 14:52:36 -0700

    wip: make top comment block a banner

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,9 @@
+import { readFileSync } from "fs";
+
 export default {
   input: "out/index.js",
   output: {
+    banner: readFileSync("banner.js", "utf8"),
     file: "dist/playlister.user.js",
     format: "cjs",
   },
