7f77ec986f34eb2a43535c49342e75f99ff739a4
Nick Derevjanik <nick@networkbasedsolutions.com>
2024-06-29 22:37:52 -0700

    wip: formatted the array jest tests

--- src/array.test.ts
+++ src/array.test.ts
@@ -1,23 +1,30 @@
 import { describe, expect, it } from "@jest/globals";
-import { chunk } from './array';
+import { chunk } from "./array";
 
-describe('chunk', () => {
-  it('should return an empty array when given an empty array', () => {
+describe(chunk.name, () => {
+  it("should return an empty array when given an empty array", () => {
     const input: number[] = [];
     const expected: number[][] = [];
     expect(chunk(input, 2)).toEqual(expected);
   });
 
-  it('should return an array of arrays when given a non-empty array', () => {
+  it("should return an array of arrays when given a non-empty array", () => {
     const input: number[] = [1, 2, 3, 4, 5, 6];
-    const expected: number[][] = [[1, 2], [3, 4], [5, 6]];
+    const expected: number[][] = [
+      [1, 2],
+      [3, 4],
+      [5, 6],
+    ];
     expect(chunk(input, 2)).toEqual(expected);
   });
 
-  it('should return an array of arrays with the specified chunk size', () => {
+  it("should return an array of arrays with the specified chunk size", () => {
     const input: number[] = [1, 2, 3, 4, 5, 6];
     const chunkSize = 4;
-    const expected: number[][] = [[1, 2, 3, 4], [5, 6]];
+    const expected: number[][] = [
+      [1, 2, 3, 4],
+      [5, 6],
+    ];
     expect(chunk(input, chunkSize)).toEqual(expected);
   });
 });
