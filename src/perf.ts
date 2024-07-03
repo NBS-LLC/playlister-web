42c2c9a82dc6ed25713e2bbce355692d6088f31e
Nick Derevjanik <nick@networkbasedsolutions.com>
2024-07-02 17:51:46 -0700

    function comment block

--- src/perf.ts
+++ src/perf.ts
@@ -1,3 +1,9 @@
+/**
+ * Measures the performance of a given asynchronous function.
+ *
+ * @param func - The function to measure the performance of.
+ * @return A promise that resolves to an object containing the return value of the function and the execution time in milliseconds.
+ */
 export async function measurePerformance<T>(
   func: () => Promise<T>,
 ): Promise<{ returnValue: T; time: number }> {
