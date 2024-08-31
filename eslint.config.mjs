875005c92f9396343a5d04b5c75b1323db61d9e7
Nick Derevjanik <nick@networkbasedsolutions.com>
2024-08-31 14:33:27 -0700

    eslint should ignore coverage dirs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ["**/dist/**", "**/out/**"],
+    ignores: ["**/coverage/**", "**/dist/**", "**/out/**"],
   },
   {
     rules: {
