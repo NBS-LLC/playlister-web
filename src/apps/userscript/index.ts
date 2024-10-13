d06b14192cdab868cf543843c8acd6ea2b2da4f7
Nick Derevjanik <nick@networkbasedsolutions.com>
2024-10-12 23:54:55 -0700

    wait for elem should not be awaited

--- src/apps/userscript/index.ts
+++ src/apps/userscript/index.ts
@@ -37,53 +37,60 @@ async function main() {
 
   // Now Playing Widget
 
-  const elemNowPlayingWidget = await waitForElem(
-    '[data-testid="now-playing-widget"]',
+  waitForElem('[data-testid="now-playing-widget"]').then(
+    async (elemNowPlayingWidget) => {
+      try {
+        updateNowPlayingWidget(
+          elemNowPlayingWidget,
+          await getCurrentlyPlayingTrackDetails(accessToken),
+        );
+      } catch (error) {
+        console.warn("Unable to update the now playing widget.");
+        console.warn("Will try again on mutation of the widget.");
+        console.warn(error);
+      }
+
+      onMutation(elemNowPlayingWidget, async function (_mutation) {
+        updateNowPlayingWidget(
+          elemNowPlayingWidget,
+          await getCurrentlyPlayingTrackDetails(accessToken),
+        );
+      });
+    },
   );
-
-  try {
-    updateNowPlayingWidget(
-      elemNowPlayingWidget,
-      await getCurrentlyPlayingTrackDetails(accessToken),
-    );
-  } catch (error) {
-    console.warn("Unable to update the now playing widget.");
-    console.warn(error);
-  }
-
-  onMutation(elemNowPlayingWidget, async function (_mutation) {
-    updateNowPlayingWidget(
-      elemNowPlayingWidget,
-      await getCurrentlyPlayingTrackDetails(accessToken),
-    );
-  });
 
   // Playlist Widget
 
-  const elemPlaylistWidget = await waitForElem(
-    '[data-testid="playlist-tracklist"]',
-  );
+  waitForElem('[data-testid="playlist-tracklist"]').then(
+    async (elemPlaylistWidget) => {
+      const currentPlaylistId = await getPlaylistId();
+      const playlistItems = await getPlaylistItems(
+        accessToken,
+        currentPlaylistId,
+      );
+      const tracks = playlistItems.map((item) => item.track);
+      const audioFeatures = await getSeveralAudioFeatures(accessToken, tracks);
 
-  const currentPlaylistId = await getPlaylistId();
-  const playlistItems = await getPlaylistItems(accessToken, currentPlaylistId);
-  const tracks = playlistItems.map((item) => item.track);
-  const audioFeatures = await getSeveralAudioFeatures(accessToken, tracks);
+      const tracksWithAudioFeatures = combinePlaylistItemsWithAudioFeatures(
+        playlistItems,
+        audioFeatures,
+      );
 
-  const tracksWithAudioFeatures = combinePlaylistItemsWithAudioFeatures(
-    playlistItems,
-    audioFeatures,
-  );
-
-  updatePlaylistWidget(elemPlaylistWidget, tracksWithAudioFeatures);
-
-  onMutation(
-    getPlaylistContainer(elemPlaylistWidget),
-    async function (_mutation) {
       updatePlaylistWidget(elemPlaylistWidget, tracksWithAudioFeatures);
+
+      onMutation(
+        getPlaylistContainer(elemPlaylistWidget),
+        async function (_mutation) {
+          updatePlaylistWidget(elemPlaylistWidget, tracksWithAudioFeatures);
+        },
+      );
     },
   );
+
+  console.log("Reach the end of main!");
 }
 
 (async function () {
-  main();
+  await main();
+  console.log("Reach the end of the userscript!");
 })();
