ffd2e31355f2ea7ac8a2f942a6870e0e14448e41
nderevj <nick@networkbasedsolutions.com>
2025-08-30 14:30:32 -0700

    poc - implemented reccobeats get-tracks api

--- src/apps/userscript/index.ts
+++ src/apps/userscript/index.ts
@@ -1,5 +1,38 @@
 import { onMutation, waitForElem } from "../../lib/html";
 
+interface Artist {
+  id: string;
+  name: string;
+  href: string;
+}
+
+interface Track {
+  id: string;
+  trackTitle: string;
+  artists: Artist[];
+  durationMs: number;
+  isrc: string;
+  ean: string;
+  upc: string;
+  href: string;
+  availableCountries?: string;
+  popularity: number;
+}
+
+async function getTracks(ids: string[]): Promise<Track[]> {
+  const baseUrl = "https://api.reccobeats.com/v1/track";
+  const params = new URLSearchParams({ ids: ids.join(",") });
+  const url = `${baseUrl}?${params.toString()}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    console.log(response);
+    throw new Error("An error occurred trying to retrieve tracks.");
+  }
+
+  return (await response.json()).content as Track[];
+}
+
 function parseNowPlayingHref(element: Element) {
   const nowPLayingHref = element?.getAttribute("href");
   return nowPLayingHref?.split(":")[2];
@@ -8,14 +41,18 @@ function parseNowPlayingHref(element: Element) {
 async function main() {
   waitForElem(
     'aside[aria-label="Now playing view"] a[href*="spotify:track:"]',
-  ).then((element) => {
+  ).then(async (element) => {
     onMutation(element, async () => {
       const trackId = parseNowPlayingHref(element);
-      console.log("now playing track id:", trackId);
+      if (trackId) {
+        console.log("now playing track:", await getTracks([trackId]));
+      }
     });
 
     const trackId = parseNowPlayingHref(element);
-    console.log("now playing track id:", trackId);
+    if (trackId) {
+      console.log("now playing track:", await getTracks([trackId]));
+    }
   });
 }
 
