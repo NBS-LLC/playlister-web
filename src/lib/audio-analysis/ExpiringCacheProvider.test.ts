01e713131db0516399db544ddd5c014eb7d0d058
nderevj <nick@networkbasedsolutions.com>
2025-10-18 18:26:53 -0700

    wip - cache provider
    * Implemented the getTrackFeatures method

--- src/lib/audio-analysis/ExpiringCacheProvider.test.ts
+++ src/lib/audio-analysis/ExpiringCacheProvider.test.ts
@@ -4,7 +4,10 @@
 
 import { AsyncObjectStorage } from "../storage/AsyncObjectStorage";
 import { LocalStorageAdapter } from "../storage/LocalStorageAdapter";
-import { GetTrackDetailsError } from "./AudioAnalysisProvider";
+import {
+  GetTrackDetailsError,
+  GetTrackFeaturesError,
+} from "./AudioAnalysisProvider";
 import { CacheExpiredError, CacheItem, NotCachedError } from "./CacheProvider";
 import { _createMockEnrichedTracks } from "./EnrichedTrack.test-data";
 import { ExpiringCacheProvider } from "./ExpiringCacheProvider";
@@ -34,6 +37,23 @@ describe(ExpiringCacheProvider.name, () => {
     });
 
     it("throws an error when the track details are not cached", async () => {
+      const cacheProvider = new ExpiringCacheProvider(storage);
+      await expect(async () => {
+        await cacheProvider.getTrackDetails("abcd1234");
+      }).rejects.toThrow(NotCachedError);
+    });
+
+    it("doesn't confuse track features for track details", async () => {
+      // Purposely create track features but NOT track details
+      const tomorrow = new Date(Date.now() + 24 * 60 * 60 * 1000);
+      const trackFeatures = _createMockEnrichedTracks()[0].features;
+      const cacheItem: CacheItem = {
+        status: "AUDIO_ANALYSIS_KNOWN",
+        data: trackFeatures,
+        expirationDateUtc: tomorrow.toISOString(),
+      };
+      localStorage.setItem("trackFeatures_abcd1234", JSON.stringify(cacheItem));
+
       const cacheProvider = new ExpiringCacheProvider(storage);
       await expect(async () => {
         await cacheProvider.getTrackDetails("abcd1234");
@@ -145,6 +165,81 @@ describe(ExpiringCacheProvider.name, () => {
       const cacheProvider = new ExpiringCacheProvider(storage);
       const result = await cacheProvider.getTrackFeatures("abcd1234");
       expect(result).toEqual(trackFeatures);
+    });
+
+    it("throws an error when the track features are not cached", async () => {
+      const cacheProvider = new ExpiringCacheProvider(storage);
+      await expect(async () => {
+        await cacheProvider.getTrackFeatures("abcd1234");
+      }).rejects.toThrow(NotCachedError);
+    });
+
+    it("doesn't confuse track details for track features", async () => {
+      // Purposely create track details but NOT track features
+      const tomorrow = new Date(Date.now() + 24 * 60 * 60 * 1000);
+      const trackDetails = _createMockEnrichedTracks()[0].details;
+      const cacheItem: CacheItem = {
+        status: "AUDIO_ANALYSIS_KNOWN",
+        data: trackDetails,
+        expirationDateUtc: tomorrow.toISOString(),
+      };
+      localStorage.setItem("trackDetails_abcd1234", JSON.stringify(cacheItem));
+
+      const cacheProvider = new ExpiringCacheProvider(storage);
+      await expect(async () => {
+        await cacheProvider.getTrackFeatures("abcd1234");
+      }).rejects.toThrow(NotCachedError);
+    });
+
+    it("throws an error when the cached data has expired", async () => {
+      const oneSecondAgo = new Date(Date.now() - 1000);
+      const trackFeatures = _createMockEnrichedTracks()[0].features;
+      const cacheItem: CacheItem = {
+        status: "AUDIO_ANALYSIS_KNOWN",
+        data: trackFeatures,
+        expirationDateUtc: oneSecondAgo.toISOString(),
+      };
+      localStorage.setItem("trackFeatures_abcd1234", JSON.stringify(cacheItem));
+
+      const cacheProvider = new ExpiringCacheProvider(storage);
+      await expect(async () => {
+        await cacheProvider.getTrackFeatures("abcd1234");
+      }).rejects.toThrow(CacheExpiredError);
+    });
+
+    it("throws an error when the cached data contains unknown audio analysis", async () => {
+      const tomorrow = new Date(Date.now() + 24 * 60 * 60 * 1000);
+      const cacheItem: CacheItem = {
+        status: "AUDIO_ANALYSIS_UNKNOWN",
+        data: null,
+        expirationDateUtc: tomorrow.toISOString(),
+      };
+      localStorage.setItem("trackFeatures_abcd1234", JSON.stringify(cacheItem));
+
+      const cacheProvider = new ExpiringCacheProvider(storage);
+      await expect(async () => {
+        await cacheProvider.getTrackFeatures("abcd1234");
+      }).rejects.toThrow(GetTrackFeaturesError);
+    });
+
+    it("removes expired cached track features", async () => {
+      const oneSecondAgo = new Date(Date.now() - 1000);
+      const trackFeatures = _createMockEnrichedTracks()[0].features;
+      const cacheItem: CacheItem = {
+        status: "AUDIO_ANALYSIS_KNOWN",
+        data: trackFeatures,
+        expirationDateUtc: oneSecondAgo.toISOString(),
+      };
+      localStorage.setItem("trackFeatures_abcd1234", JSON.stringify(cacheItem));
+
+      const cacheProvider = new ExpiringCacheProvider(storage);
+      try {
+        await cacheProvider.getTrackFeatures("abcd1234");
+      } catch {
+        /* Do Nothing */
+      }
+
+      expect(localStorage.getItem("trackFeatures_abcd1234")).toBeNull();
     });
   });
 });
