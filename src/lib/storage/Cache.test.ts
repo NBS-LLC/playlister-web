03af0193ebf7667ea2dfc05e4ed69c2df1a0a581
nderevj <nick@networkbasedsolutions.com>
2025-10-28 12:46:40 -0700

    replaced hardcoded test name

--- src/lib/storage/Cache.test.ts
+++ src/lib/storage/Cache.test.ts
@@ -84,7 +84,7 @@ describe(Cache.name, () => {
     });
   });
 
-  describe("store", () => {
+  describe(Cache.prototype.store.name, () => {
     beforeAll(() => {
       jest.useFakeTimers();
       jest.setSystemTime(new Date("2024-01-01T00:00:00.000Z"));
