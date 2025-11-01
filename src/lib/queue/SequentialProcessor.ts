155f146d53c245943397bc0481f7e4a488353ef8
nderevj <nick@networkbasedsolutions.com>
2025-11-01 14:48:38 -0700

    added a seq processor queue

---
+++ src/lib/queue/SequentialProcessor.ts
@@ -0,0 +1,45 @@
+import { delay } from "../delay";
+
+type ProcessFunction<T> = (element: T) => Promise<void>;
+
+export class SequentialProcessor<T> {
+  private queue: T[] = [];
+  private isProcessing: boolean = false;
+  private readonly minDelayMs: number;
+  private readonly processFn: ProcessFunction<T>;
+
+  constructor(processFn: ProcessFunction<T>, minDelayMs: number) {
+    this.processFn = processFn;
+    this.minDelayMs = minDelayMs;
+  }
+
+  public enqueue(element: T): void {
+    this.queue.push(element);
+    this.startProcess();
+  }
+
+  private async startProcess(): Promise<void> {
+    if (this.isProcessing) {
+      return;
+    }
+
+    this.isProcessing = true;
+
+    while (this.queue.length > 0) {
+      const element = this.queue.shift();
+
+      if (element) {
+        try {
+          await this.processFn(element);
+        } catch (error) {
+          console.warn(error);
+          // Continue processing the rest of the queue.
+        }
+      }
+
+      await delay(this.minDelayMs);
+    }
+
+    this.isProcessing = false;
+  }
+}
