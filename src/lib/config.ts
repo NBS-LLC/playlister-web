90e30769d0b6b060e415462e9e632fdbff530e35
nderevj <nick@networkbasedsolutions.com>
2025-11-04 17:22:10 -0800

    defined quota max and target

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,11 @@
+const MB = 1024 * 1024;
+
 export const config = {
   appName: "",
   appId: "",
+
+  cacheQuotaMaxBytes: 5 * MB,
+  cacheQuotaTargetBytes: 2.5 * MB,
 
   get namespace() {
     return config.appId ? `${config.appId}:` : "";
