8a4ebe6d330532d4a6b4ce72fd0b4df412ad7d18
nderevj <nick@networkbasedsolutions.com>
2025-11-05 16:51:52 -0800

    ignore coverage of test-data

--- src/lib/jest.config.mjs
+++ src/lib/jest.config.mjs
@@ -1,6 +1,7 @@
 export default {
   collectCoverage: true,
   collectCoverageFrom: ["**/*.ts"],
+  coveragePathIgnorePatterns: ["/node_modules/", ".test-data.ts"],
   preset: "ts-jest",
   setupFilesAfterEnv: ["./jest.setup.ts"],
   verbose: true,
