ba097d5b0d061a71a1cf7831709d1bb72bd2bb1a
nderevj <nick@networkbasedsolutions.com>
2025-11-22 15:46:45 -0800

    updated project names

--- src/apps/userscript/rollup.config.mjs
+++ src/apps/userscript/rollup.config.mjs
@@ -11,7 +11,7 @@ export default {
   input: "out/apps/userscript/index.js",
   output: {
     banner: readFileSync("banner.js", "utf8"),
-    file: "dist/playlister.user.js",
+    file: "dist/spotavibe-lite.user.js",
     format: "es",
   },
   plugins: [
